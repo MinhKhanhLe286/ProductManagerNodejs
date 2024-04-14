2abfd574befdbea860768afcb931eaf07fcb390f
Minh Khanh Le <minhkhanh250204z@gmail.com>
2024-04-14 12:45:47 +0700

    config controller

---
+++ controller/client/product.controller.js
@@ -0,0 +1,3 @@
+module.exports.index=(rep,res)=>{
+    res.render("client/pages/products/index.pug")
+}
--- routers/clients/product.router.js
+++ routers/clients/product.router.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/",(rep,res)=>{
-    res.render("client/pages/products/index.pug")
-})
+const controller = require("../../controller/client/product.controller.js")
+router.get("/",controller.index)
 
 module.exports = router
