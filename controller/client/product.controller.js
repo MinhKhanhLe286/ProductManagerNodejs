085c47a234dbf7a0a172c3b076296b86aa042805
Minh Khanh Le <minhkhanh250204z@gmail.com>
2024-04-15 22:25:50 +0700

    config dattabase

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,5 +1,14 @@
-module.exports.index=(rep,res)=>{
+const Product = require("../../model/product.model")
+
+module.exports.index=async(rep,res)=>{
+
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    console.log(products)
     res.render("client/pages/products/index.pug",{
-        titlePage : "Product Home"
+        titlePage : "Product Home",
+        products : products
     })
 }
--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require("express")
-require("dotenv").config()
+const express = require("express");
 
+require("dotenv").config();
+
+const database = require("./config/database")
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+database.connect();
 
-const route = require("./routers/clients/index.router")
+const route = require("./routers/clients/index.router");
+const { connect } = require("mongoose");
 
-// cấu hình pug
-app.set("views","./views")
-app.set("view engine","pug")
-//router
+// Cấu hình pug
+app.set("views", "./views");
+app.set("view engine", "pug");
 
+// Router
 route(app);
 
-app.listen(port,()=>{
-    console.log("app listening on port "+port)
-})
+app.listen(port, () => {
+    console.log("App listening on port " + port);
+});
